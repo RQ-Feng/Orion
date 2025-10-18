bc5b06589cb6799f2578e6453afb6a6b594e4ef1
RQ_Feng <mrfeng1234561112022@gmail.com>
2025-10-18 23:21:13 +0800

    update Setting.lua

--- Other-script/Setting.lua
+++ Other-script/Setting.lua
@@ -1,13 +1,28 @@
 --Need OrionLib
-Setting = Window:MakeTab({
-    Name = "UI设置",
-    Icon = "rbxassetid://4483345998"
-})
-Setting:AddButton({
-    Name = "关闭UI",
-    Callback = function() OrionLib:Destroy() end
-})
-Setting:AddLabel("此服务器上的游戏ID为:" .. game.GameId)
-Setting:AddLabel("此服务器位置ID为:" .. game.PlaceId)
-Setting:AddParagraph("此服务器UUID为:", game.JobId)
-Setting:AddLabel("此服务器上的游戏版本为:version_" .. game.PlaceVersion)
+for _,MainWindow in pairs(OrionLib.MainWindows) do
+    Setting = MainWindow:MakeTab({
+        Name = "UI设置",
+        Icon = "rbxassetid://4483345998"
+    })
+    Setting:AddButton({
+        Name = "关闭UI",
+        Callback = function() OrionLib:Destroy() end
+    })
+    local Themes = {
+        ['暗色'] = "Dark",
+        ['浅色'] = "Light"
+    }
+    Setting:AddDropdown({
+        Name = "UI主题",
+        Default = "暗色",
+        Options = {"暗色","浅色"},
+        Callback = function(Value)
+            if not Themes[Value] then return end
+            OrionLib:SetTheme(Themes[Value])
+        end    
+    })
+    Setting:AddLabel("此服务器上的游戏ID为:" .. game.GameId)
+    Setting:AddLabel("此服务器位置ID为:" .. game.PlaceId)
+    Setting:AddParagraph("此服务器UUID为:", game.JobId)
+    Setting:AddLabel("此服务器上的游戏版本为:version_" .. game.PlaceVersion)
+end
