3121e39c47e4481d9d4af454cffeeaf225aac18b
RQ_Feng <mrfeng1234561112022@gmail.com>
2025-11-01 13:26:55 +0800

    update readme,Localization table and modify file tree structure

---
+++ Other-scripts/Localization.lua
@@ -0,0 +1,15 @@
+return {
+    ['zh-cn'] = {
+        ['OrionLib.InterfaceHidden.Name'] = '界面隐藏',
+        ['OrionLib.InterfaceHidden.Content.Mobile'] = '点击图标以重新打开界面',
+        ['OrionLib.InterfaceHidden.Content.Computer'] = '按右Shift以重新打开界面',
+        ['OrionLib.InterfaceHidden.Content.Computer.ShiftAgain'] = '再次按右Shift以重新打开界面',
+        ['OrionLib.Configuration.Name'] = '游戏配置'
+    },
+    ['en-us'] = {
+        ['OrionLib.InterfaceHidden.Name'] = 'Interface Hidden',
+        ['OrionLib.InterfaceHidden.Content.Mobile'] = 'Click the icon to reopen the interface',
+        ['OrionLib.InterfaceHidden.Content.Computer'] = 'Tap rightShift to reopen the interface',
+        ['OrionLib.InterfaceHidden.Content.Computer.ShiftAgain'] = 'Tap rightShift again to reopen the interface'
+    }
+}
--- README.md
+++ README.md
@@ -7,10 +7,6 @@ Maybe has some problem bc my luau isn't very good
 
 # Lib loader:
 ```lua
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua"))()
-```
-If you want to use ui setting or something else,you need to set Orion as a global variable:
-```lua
 OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua"))()
 ```
 ![Thread](https://user-images.githubusercontent.com/77512805/164973978-31f19af4-528a-4af0-9ba8-21bc22e668ef.png)
